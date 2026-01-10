712b7e58ea2eb298cc0eb1b6ad8958c21161e0c1
RafyWP <rafy@rafy.com.br>
2026-01-10 18:07:35 -0300

    feat: setup Jest and Testing Library for unit tests

---
+++ __tests__/utils.test.ts
@@ -0,0 +1,9 @@
+// Example test for the cn utility
+import { cn } from '@/lib/utils'
+
+describe('cn utility', () => {
+  it('merges class names correctly', () => {
+    expect(cn('class1', 'class2')).toBe('class1 class2')
+    expect(cn('class1', undefined, 'class2')).toBe('class1 class2')
+  })
+})
