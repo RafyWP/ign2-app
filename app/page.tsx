7aa2ef069c597766759a9556dbe1ef64d2177486
RafyWP <rafy@rafy.com.br>
2026-01-11 12:55:19 -0300

    Aumentar fonte do h1 para text-4xl e definir largura 100%

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-col items-center justify-between gap-6 py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="w-full flex flex-col items-center gap-6 text-center">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="w-full text-4xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             {t('home.title')}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
