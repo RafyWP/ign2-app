718d124c0274d4601dff16b1dcfc99cca9a9bb46
RafyWP <rafy@rafy.com.br>
2026-01-11 12:53:07 -0300

    Definir largura 100% para os divs dentro do main

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex w-full max-w-3xl flex-col items-center justify-between gap-6 py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center">
+        <div className="w-full flex flex-col items-center gap-6 text-center">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             {t('home.title')}
           </h1>
@@ -16,7 +16,7 @@ export default function Home() {
             {t('home.description')}
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium justify-center sm:flex-row">
+        <div className="w-full flex flex-col gap-4 text-base font-medium justify-center sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:min-w-[200px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
